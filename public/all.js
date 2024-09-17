7821cf9991cd35c5aace8a81a2485bc1a3628a2f
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-09-17 15:23:27 +0200

    feat: add cancel button to payemnt request

--- public/all.js
+++ public/all.js
@@ -1,70 +1,6 @@
-const appendMessage = (message) => {
-  const messageDiv = document.createElement("div");
-  messageDiv.className = "message";
-  messageDiv.textContent = message;
-  output.appendChild(messageDiv);
-};
-
-function insertQRCode(base64String, placeholderSelector) {
-  const img = document.createElement("img");
-  img.src = base64String;
-
-  const qrCodePlaceholder = document.querySelector(placeholderSelector);
-
-  qrCodePlaceholder.innerHTML = "";
-  qrCodePlaceholder.appendChild(img);
-}
-
-function toggleVisibleClass(className, isVisible) {
-  const element = document.querySelector(`.${className}`);
-  if (element) {
-    if (isVisible) {
-      element.classList.add("visible");
-    } else {
-      element.classList.remove("visible");
-    }
-  }
-}
-
-const initializeUi = () => {
-  document.getElementById("item-price-fiat").textContent = "-";
-  document.getElementById("item-price-sat").textContent = "-";
-  document.getElementById("item-model").textContent = "-";
-  toggleVisibleClass("container-instructions", true);
-  toggleVisibleClass("container-payment-request", false);
-  toggleVisibleClass("container-success", false);
-  toggleVisibleClass("container-connecting", false);
-};
-
-const setScreenPaymentRequest = (qrCodeBase64, fiatAmount, sat, itemNumber) => {
-  document.getElementById("item-price-fiat").textContent = fiatAmount;
-  document.getElementById("item-price-sat").textContent = sat;
-  document.getElementById("item-model").textContent = itemNumber;
-  insertQRCode(qrCodeBase64, ".qr-code");
-
-  toggleVisibleClass("container-payment-request", true);
-  toggleVisibleClass("container-instructions", false);
-  toggleVisibleClass("container-success", false);
-  toggleVisibleClass("container-connecting", false);
-};
-
-const setScreenSuccess = () => {
-  toggleVisibleClass("container-success", true);
-  toggleVisibleClass("container-instructions", false);
-  toggleVisibleClass("container-payment-request", false);
-  toggleVisibleClass("container-connecting", false);
-};
-
-const setScreenConnecting = () => {
-  toggleVisibleClass("container-connecting", true);
-  toggleVisibleClass("container-success", false);
-  toggleVisibleClass("container-instructions", false);
-  toggleVisibleClass("container-payment-request", false);
-};
-
-initializeUi();
-
 const ws = new WebSocket("ws://" + window.location.host);
+
+let itWasOnceConnected = false;
 
 ws.onerror = (error) => {
   console.error(error);
@@ -72,6 +8,10 @@ ws.onerror = (error) => {
 
 ws.onopen = () => {
   appendMessage("Connected to WebSocket server");
+  if (!itWasOnceConnected) {
+    itWasOnceConnected = true;
+    setScreenInstructions();
+  }
 };
 
 ws.onmessage = (event) => {
@@ -107,12 +47,100 @@ ws.onclose = () => {
   appendMessage("Disconnected from WebSocket server");
 };
 
+const requestInitializeVendCycle = () => {
+  ws.send(
+    JSON.stringify({
+      type: "command",
+      data: { command: "C,START,0" },
+    })
+  );
+};
+
+const appendMessage = (message) => {
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message";
+  messageDiv.textContent = message;
+  output.appendChild(messageDiv);
+};
+
+function insertQRCode(base64String, placeholderSelector) {
+  const img = document.createElement("img");
+  img.src = base64String;
+
+  const qrCodePlaceholder = document.querySelector(placeholderSelector);
+
+  qrCodePlaceholder.innerHTML = "";
+  qrCodePlaceholder.appendChild(img);
+}
+
+function toggleVisibleClass(className, isVisible) {
+  const element = document.querySelector(`.${className}`);
+  if (element) {
+    if (isVisible) {
+      element.classList.add("visible");
+    } else {
+      element.classList.remove("visible");
+    }
+  }
+}
+
+const setScreenInstructions = () => {
+  setTimeout(() => {
+    requestInitializeVendCycle();
+  }, 1000);
+  document.getElementById("item-price-fiat").textContent = "-";
+  document.getElementById("item-price-sat").textContent = "-";
+  document.getElementById("item-model").textContent = "-";
+  toggleVisibleClass("container-instructions", true);
+  toggleVisibleClass("container-payment-request", false);
+  toggleVisibleClass("container-success", false);
+  toggleVisibleClass("container-connecting", false);
+};
+
+const setScreenPaymentRequest = (qrCodeBase64, fiatAmount, sat, itemNumber) => {
+  document.getElementById("item-price-fiat").textContent = fiatAmount;
+  document.getElementById("item-price-sat").textContent = sat;
+  document.getElementById("item-model").textContent = itemNumber;
+  insertQRCode(qrCodeBase64, ".qr-code");
+
+  toggleVisibleClass("container-payment-request", true);
+  toggleVisibleClass("container-instructions", false);
+  toggleVisibleClass("container-success", false);
+  toggleVisibleClass("container-connecting", false);
+};
+
+const setScreenSuccess = () => {
+  toggleVisibleClass("container-success", true);
+  toggleVisibleClass("container-instructions", false);
+  toggleVisibleClass("container-payment-request", false);
+  toggleVisibleClass("container-connecting", false);
+};
+
+const setScreenConnecting = () => {
+  toggleVisibleClass("container-connecting", true);
+  toggleVisibleClass("container-success", false);
+  toggleVisibleClass("container-instructions", false);
+  toggleVisibleClass("container-payment-request", false);
+};
+
+setScreenConnecting();
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     switch (event.target.id) {
+      case "cancelPaymentRequest":
+        console.log("cancelPaymentRequest");
+        ws.send(
+          JSON.stringify({
+            type: "command",
+            data: { command: "C,STOP" },
+          })
+        );
+        setScreenInstructions();
+        break;
       case "cancelCommand":
-        console.log("disableCommand");
+        console.log("cancelCommand");
         ws.send(
           JSON.stringify({
             type: "command",
@@ -149,6 +177,7 @@ buttons.forEach((button) => {
         break;
       case "command":
         const command = document.getElementById("commandValue").value;
+        console.log("Sending command", command);
         ws.send(
           JSON.stringify({
             type: "command",
